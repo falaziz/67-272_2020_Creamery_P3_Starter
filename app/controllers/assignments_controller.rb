85f4a9c6cb9ae8d8b855bd794c1d2d70f9a22a88
falaziz <muhamma2@andrew.cmu.edu>
2020-04-01 22:49:50 +0000

    base controllers intiated, the third time!

---
+++ app/controllers/assignments_controller.rb
@@ -0,0 +1,53 @@
+class AssignmentsController < ApplicationController
+    before_action :set_assignment, only: [:show, :edit, :update, :destroy]
+    
+    def terminate
+        @assignment.end_date = Date.today
+    end
+    
+    def index
+        @assignments = Assignment.chronological.paginate(:page => params[:page]).per_page(5)
+    end
+    
+    def show
+    end
+    
+    def new
+        @assignment = Assignment.new
+    end
+    
+    def create
+        @assignment = Assignment.new(assignment_params)
+        if @assignment.save
+            redirect_to assignment_path(@assignment), notice: "#{@assignment.name} was added to the system."
+        else
+            render action: 'new'
+        end
+    end
+    
+    def edit
+        @assignment = Assignment.find(params[:id])
+    end
+    
+    def update
+        if @assignment.update
+            redirect_to assignment_path(@assignment), notice: "#{@assignment.name} was revised in the system."
+        else
+            render action: 'edit'
+        end
+    end
+    
+    def destroy
+        @assignment.destroy
+        redirect_to assignments_url
+    end
+    
+    private
+    def set_assignment
+        @assignment= Assignment.find(params[:id])
+    end
+    
+    def assignment_params
+        params.require(:assignment).permit(:store_id, :employee_id, :start_date)
+    end
+end
---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,16 @@
+class EmployeesController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
